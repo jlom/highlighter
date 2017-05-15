c437914fde58c9e328c557cd7471f93a5fa2dc48
Jørgen Arnor G. Lom <j@lom.me>
2017-05-16 00:34:33 +0200

    Adds formbucket

--- src/js/contactform.js
+++ src/js/contactform.js
@@ -1,7 +1,7 @@
 export default function contactform(selector){
 
     const typeInput = document.querySelector(selector + ' #contacttype'),
-
+        form = document.querySelector(selector),
         projectHTML = `
             <fieldset id="project" class="contactform_project">
                 <label>Budsjett (circa)</label>
@@ -12,13 +12,13 @@ export default function contactform(selector){
                     <input id="timeframe-asap" type="radio" name="timeframe" value="ASAP" />
                     <label for="timeframe-asap">I går</label>
 
-                    <input id="timeframe-medium" type="radio" name="timeframe" value="medium" checked />
+                    <input id="timeframe-medium" type="radio" name="timeframe" value="weeks" />
                     <label for="timeframe-medium">Uker</label>
 
-                    <input id="timeframe-long" type="radio" name="timeframe" value="long" checked />
+                    <input id="timeframe-long" type="radio" name="timeframe" value="months" checked />
                     <label for="timeframe-long">Måneder</label>
 
-                    <input id="timeframe-xlong" type="radio" name="timeframe" value="long" />
+                    <input id="timeframe-xlong" type="radio" name="timeframe" value="years" />
                     <label for="timeframe-xlong">År</label>
                 </div>
             </fieldset>
@@ -76,6 +76,47 @@ export default function contactform(selector){
                 element.remove();
             }, 1000)
         }
+
     });
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+            const elems = form.querySelectorAll('input:not([type=radio]), select, textarea, input[type=radio]:checked'),
+                range = document.getElementById('range');
+            let formData = {};
+
+            [].forEach.call(elems, element => {
+                let key = element.name,
+                    value = element.value;
+                if(key){
+                    formData[key] = value;
+                }
+            });
+
+            if(range){
+                formData['ballparkFrom'] = range.noUiSlider.get()[0].replace('&nbsp;', ' ');
+                formData['ballparkTo'] = range.noUiSlider.get()[1].replace('&nbsp;', ' ');
+            }
+
+            form.setAttribute('aria-busy', 'true')
+
+            fetch('https://api.formbucket.com/f/buk_VORbfNwPT2PdQ0rd33BKPLxN', {
+                method: 'post',
+                mode: 'cors',
+                headers: {
+                    'accept' : 'application/javascript',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            }).then(d => {
+                form.setAttribute('aria-busy', 'false');
+                form.classList.add('sent');
+            }).catch(err => {
+                form.setAttribute('aria-busy', 'false');
+                form.classList.add('failed');
+            });
+
+        return false;
+    });
 }
